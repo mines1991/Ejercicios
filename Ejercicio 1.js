f9f0311a4d45175e9250fa2807e58a55fb4a3ef4
mines1991 <89265014+mines1991@users.noreply.github.com>
2022-05-03 11:56:34 -0300

    Add files via upload

---
+++ Ejercicio 1.js
@@ -0,0 +1,8 @@
+//Declara una variable que se llame nombre
+//el dato que pasamos a la variable se lo pedimos al usuario en un prompt
+//mostrarmos una alerta con la cantidad de caracteres que ingresa el usuario
+
+let nombre = prompt ("Ingrese su nombre");
+
+
+alert ("Su nombre tiene" + " " + nombre.length + " " + "caracteres");
